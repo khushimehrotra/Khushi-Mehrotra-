763b14ec16359948f7059522e6b6bcf3ba536892
Khushi <khushimehrotra2401@gmail.com>
2025-09-11 17:04:31 +0530

    experiment 1&2

---
+++ Q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+//Q1. WAP to calculate the area and perimeter of a rectangle based on its length and width
+int main() {
+    int length = 2 , width = 4;
+    printf("Area of rectangle : %d" , length*width);
+    printf("\n Perimeter of rectangle : %d" , 2*(length + width));
+    return 0;
+
+}
+    
---
+++ Q3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    //Q2. WAP to convert temperature from celsius to fahrenheit using the formula F= (C*9/5) +32
+
+
+    int C = 32 , F = (C * 9/5) + 32;
+    printf("\n celsius value is : %d" , C);
+    printf("\n fahrenheit value is : %d" , F);
+    
+}
